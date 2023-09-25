eaabc0ba7bd6d02537a9176965de1715f92a11f5
EmekadeFirst <119001445+emekadefirst@users.noreply.github.com>
2023-09-25 01:45:20 +0100

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,13 +1,12 @@
-from whitenoise import WhiteNoise
+# from whitenoise import WhiteNoise
+
+# application = WhiteNoise(application, root="staticfiles")
+# application.add_files("staticfiles", prefix="more-files/")
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-# Create the Django application
 application = get_wsgi_application()
-
-# Configure WhiteNoise to serve static files
-application = WhiteNoise(application, root="staticfiles")
-application.add_files("staticfiles", prefix="more-files/")
